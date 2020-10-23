245a450a3dec461179c9b2c88a097c0369791e65
Hammad Aslam <hammadaslam382@gmail.com>
2020-10-23 21:42:12 +0500

    Update today.js

--- src/today.js
+++ src/today.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Today () {
     return(
         <div>
-            <h1>Today we learn React</h1>
+            <h1>Today we learn React <br /> And tommorow we learn Javascript.</h1>
         </div>
     )
 }
